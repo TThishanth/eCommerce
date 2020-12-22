3e88f1b657b8643b98aa4eadbee0933760a4dcc5
TThishanth <thishanth41@gmail.com>
2020-12-22 10:43:57 +0530

    Final Commit

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,6 +1,5 @@
 import 'package:eCommerce/services/auth_services.dart';
 import 'package:eCommerce/widgets/progress_widget.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
